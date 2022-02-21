fb3b9659e3f4e2e2a467e7530438b77ab8c04f3e
Chubak <chubakbiddpaa@gmail.com>
2022-02-21 07:14:22 +0330

    implemented nn

--- algorithms/simple.py
+++ algorithms/simple.py
@@ -1,9 +1,10 @@
 from .distance import return_distance
 import numpy as np
 import pandas as pd
+from typing import Any
 
 
-def distance_to_all_points(dataset: np.array, distance_name: str, minowski_norm=2) -> pd.DataFrame:
+def distance_to_all_points(dataset: np.array, distance_name: str, minowski_norm=2) -> pd.DataFrame and Any:
     distance_func = return_distance(distance_name)
 
     match distance_name:
@@ -30,4 +31,35 @@ def distance_to_all_points(dataset: np.array, distance_name: str, minowski_norm=
 
     ret_df = pd.DataFrame.from_records(mapped_values, columns=["Metric", "Sum_of_Distances"])
 
-    return ret_df
+    return ret_df, ret_df.iloc[ret_df.idxmax(), :]
+
+
+def distance_to_nearest_neighbor(dataset: np.array, distance_name: str, minowski_norm=2) -> pd.DataFrame and Any:
+    distance_func = return_distance(distance_name)
+
+    match distance_name:
+        case 'mahalanobis':
+            covariance_matrix = np.cov(dataset)
+            dist_lambda = lambda x, y: distance_func(x, y, covariance_matrix)
+        case "minowski":
+            dist_lambda = lambda x, y: distance_func(x, y, minowski_norm)
+        case _:
+            dist_lambda = lambda x, y: distance_func(x, y)
+
+    mapped_values = []
+
+    def func_map(x: float, y: np.array):
+        lambda_x = lambda y_: dist_lambda(x, y_)
+
+        y_ret = np.vectorize(lambda_x)(y[y != x])
+
+        mapped_values.append((x, np.min(y_ret)))
+
+    lambda_y = lambda x: func_map(x, dataset)
+
+    np.vectorize(lambda_y)(dataset)
+
+    ret_df = pd.DataFrame.from_records(mapped_values, columns=["Metric", "NN_Distance"])
+
+    return ret_df, ret_df.iloc[ret_df.idxmax(), :]
+
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from algorithms.simple import distance_to_all_points
+from algorithms.simple import distance_to_all_points, distance_to_nearest_neighbor
 import numpy as np
 
 
@@ -7,6 +7,18 @@ def test_distances_to_all_points():
 
     distance_name = "mahalanobis"
 
-    return_df = distance_to_all_points(dataset, distance_name)
+    return_df, max_ = distance_to_all_points(dataset, distance_name)
 
+    print(max)
+    print(return_df.head())
+
+
+def test_distances_to_nearest_neighbor():
+    dataset = np.array([1, 2, 3, 8, 20, 21])
+
+    distance_name = "euclidean"
+
+    return_df, max_ = distance_to_nearest_neighbor(dataset, distance_name)
+
+    print(max_)
     print(return_df.head())
